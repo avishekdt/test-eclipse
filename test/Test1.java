b4907409b289a450e2431731685971b377692ad1
avishekdt <adatta.mid@gmail.com>
2020-10-11 22:29:52 +0530

    added testng file

---
+++ test/Test1.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void f2() {
+	  System.out.println("testing");
+  }
+  @Test
+  public void f1() {
+	  System.out.println("testing");
+  }
+  @Test
+  public void f3() {
+	  System.out.println("testing");
+  }
+}
